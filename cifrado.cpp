d24650ed3044fa0831edd50d03409c997e4c808f
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-06-21 23:50:52 -0500

    Segunda modificacion

--- cifrado.cpp
+++ cifrado.cpp
@@ -8,55 +8,126 @@
 #include "cifrado.h"
 using namespace std;
 
-void EscribirllaveMatrix(size_t n, T1 llave[10][10]){
-  ofstream outFile("llave_matriz.txt");
-    for (size_t i=0; i<n; i++){
-        for (size_t j=0; j<0; j++){
-            outFile<<llave[i][j]<<" ";
+void EscribirllaveMatrix(size_t n, T1 llave[10][10])
+{
+    ofstream outFile("llave_matriz.txt");
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < 0; j++)
+        {
+            outFile << llave[i][j] << " ";
         }
-        outFile<<endl;
+        outFile << endl;
     }
     outFile.close();
 }
-void LeerllaveMatrix(size_t n, T1 llave[10][10]){
-      ifstream inFile("llave_matriz.txt");
-    for (size_t i=0; i<n; i++){
-        for (size_t j=0; j<n; j++){
-            inFile>>llave[i][j];
+void LeerllaveMatrix(size_t n, T1 llave[10][10])
+{
+    ifstream inFile("llave_matriz.txt");
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            inFile >> llave[i][j];
         }
     }
     inFile.close();
 }
 
-//Letra a num
-void Cambio_A(string mensaje , T1 mensajeNum[200]){
-    for (size_t i=0; i<mensaje.length(); i++){
-        (mensaje[i]>='A' &&  mensaje[i] <= 'Z')? mensajeNum[i]=mensaje[i]- 'A': mensajeNum[i]=23;
-    }
-}
-
-void Cambio_a(string mensaje,T1 mensajeNum[200]){
-    for (size_t i=0; i<mensaje.length(); i++){
-      (mensaje[i]>='a' &&  mensaje[i] <= 'z')?  mensajeNum[i]=mensaje[i]- 'a': mensajeNum[i]=23;
-    }
-    }
-
-void letraNumero(string mensaje, T1 mensajeNum[200], size_t ope){
-    void (*palabra[2])(string, T1[200])={Cambio_A, Cambio_a};
+// Letra a num
+void Cambio_A(string mensaje, T1 mensajeNum[200])
+{
+    for (size_t i = 0; i < mensaje.length(); i++)
     {
-            palabra[ope](mensaje, mensajeNum);
-   }
+        (mensaje[i] >= 'A' && mensaje[i] <= 'Z') ? mensajeNum[i] = mensaje[i] - 'A' : mensajeNum[i] = 23;
+    }
 }
 
+void Cambio_a(string mensaje, T1 mensajeNum[200])
+{
+    for (size_t i = 0; i < mensaje.length(); i++)
+    {
+        (mensaje[i] >= 'a' && mensaje[i] <= 'z') ? mensajeNum[i] = mensaje[i] - 'a' : mensajeNum[i] = 23;
+    }
+}
 
+void letraNumero(string mensaje, T1 mensajeNum[200], size_t ope)
+{
+    void (*palabra[2])(string, T1[200]) = {Cambio_A, Cambio_a};
+    {
+        palabra[ope](mensaje, mensajeNum);
+    }
+}
 
+void numeroLetra(T1 mensajeCifrado[200], T1 total)
+{
+    ofstream outfile("mensaje_cifrado.txt", ios::app);
+    for (size_t i = 0; i < total; i++)
+    {
+        outfile << T3(mensajeCifrado[i] + 'A');
+    }
+    outfile << endl;
+    outfile.close();
+}
 
+T1 Particion(size_t n, string mensaje, T1 mensajeNum[200])
+{
+    size_t longitud = (size_t)mensaje.length();
+    size_t padding = (n - (longitud % n)) % n;
+    for (size_t i = 0; i < padding; i++)
+    {
+        mensajeNum[longitud + i] = 23;
+    }
+    return longitud + padding;
+}
 
+T1 CrearMensajeNum(size_t n, string mensaje, T1 mensajeNum[200])
+{
+    letraNumero(mensaje, mensajeNum, 0);
+    return Particion(n, mensaje, mensajeNum);
+}
 
-void DemoCifrado(){
-    cout<<"Escribe la llave de la matriz";
-    T1 n=0;
-    T1 llave[10][10];
-    EscribirllaveMatrix(n, llave);
+void PrintMatrixmensajeNum(size_t n, T1 total, T1 mensajeNum[])
+{
+    ofstream outFile("mensaje_cifrado.txt");
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            mensajeNum[i][j] = mensajeNum[i * n + j];
+            outFile << mensajeNum[i * n + j] << " ";
+        }
+        outFile << endl;
+    }
+    outFile.close();
+}
+
+void MCifrado(size_t n, T1 llave[10][10], T1 mensajeNum[200], T1 total, T1 mensajeCifrado[200])
+{
+    ofstream cipherFile("mensaje_cifrado.txt");
+    T1 k = total / n;
+    for (size_t i = 0; i < k; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            mensajeCifrado[i * n + j] = 0;
+            for (size_t l = 0; l < n; l++)
+            {
+                mensajeCifrado[i * n + j] += llave[j][l] * mensajeNum[i * n + l];
+            }
+            mensajeCifrado[i * n + j] = ((mensajeCifrado[i * n + j] % 26) + 26) % 26;
+        }
+    }
+    cipherFile << "Mensaje cifrado es:" << endl;
+    numeroLetra(mensajeCifrado, total);
+    cipherFile.close();
+}
+
+void LlaveMatrix(size_t n, T1 llave[10][10], ostream &salida, istream &entrada)
+{
+    salida << "Ingrese el tamanio de la llave matriz (n x n): ";
+    entrada >> n;
     LeerllaveMatrix(n, llave);
+    salida << endl;
+    EscribirllaveMatrix(n, llave);
 }
--- cifrado.h
+++ cifrado.h
@@ -4,11 +4,20 @@
 #include <cstddef>
 #include <string>
 
-void leerllaveMatrix(size_t n,T1 llave[10][10]);
+void LeerllaveMatrix(size_t n,T1 llave[10][10]);
 void EscribirllaveMatrix(size_t n, T1 llave[10][10]);
-void llaveMatrix(string mensaje, T1 mensajeNum[200], size_t ope);
+void llaveMatrix(string mensaje, T1 mensajeNum[200], size_t ope, ostream &salida, istream &entrada);
 
-void DemoCifrado();
+//Convertidor
+void numeroLetra(T1 mensajeNum[200], string &mensaje, size_t longitud);
+void letraNumero(string mensaje, T1 mensajeNum[200], size_t ope);
+
+T1 Particion(size_t n, string mensaje, T1 mensajeNum[200]);
+T1 CrearMensajeNum(size_t n, string mensaje, T1 mensajeNum[200]);
+void PrintMatrixmensajeNum(size_t n, T1 total, T1 mensajeNum[]);
+
+void MCifrado(size_t n, T1 llave[10][10], T1 mensajeNum[200], T1 total, T1 mensajeCifrado[200]);
 
 
+    
 #endif
